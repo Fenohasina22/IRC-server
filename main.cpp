79eade0893168857e8a19b6342bd5d7b930956b6
Fenohasina Samy Andriatafitantsoa <fsamy-an@student.42antananarivo.mg>
2026-04-06 13:32:10 +0300

    Added more documentations

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mratsima <mratsima@student.42antananari    +#+  +:+       +#+        */
+/*   By: fsamy-an <fsamy-an@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/04 15:02:43 by mratsima          #+#    #+#             */
-/*   Updated: 2026/04/04 15:12:46 by mratsima         ###   ########.fr       */
+/*   Updated: 2026/04/06 13:17:24 by fsamy-an         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,13 @@ int main(int argc, char **argv)
 		return (1);
 	std::cout << "password: " << argv[1] <<  std::endl;
 	std::cout << "port: " 	  << argv[2] <<  std::endl;
+
+
+	/*
+		I need to create a main loop using poll()
+	
+	*/
+
+
+	return (0);
 }
