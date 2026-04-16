86b358976a78b6f5086fc926f0299e2aae000ecb
mratsima <mratsima@student.42antananarivo.mg>
2026-04-16 15:19:19 +0300

    cleaned part

--- commands.cpp
+++ commands.cpp
@@ -6,7 +6,7 @@
 /*   By: mratsima <mratsima@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/07 16:48:57 by mratsima          #+#    #+#             */
-/*   Updated: 2026/04/16 15:13:13 by mratsima         ###   ########.fr       */
+/*   Updated: 2026/04/16 15:19:10 by mratsima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -188,24 +188,22 @@ bool	partCmd(Client &client, iRCMessage &mess, Server &serv)
 
 	if (mess.args.empty())
 	{
-		client.ConcatenateWBuffer(FormatedMessage("461", ":server", "* PART :Not enough parameters"), serv);
+		client.ConcatenateWBuffer(FormatedMessage("461", ":server",
+			 "* PART :Not enough parameters"), serv);
 		return (false);
 	}
-	// Check:
-	// channel exists
 	if (!chanExists(mess.args[0], serv))
 	{
-		client.ConcatenateWBuffer(FormatedMessage("403", ":server", client.getNick() + " " + mess.args[0] + " :No such channel"), serv);
+		client.ConcatenateWBuffer(FormatedMessage("403", ":server",
+			 client.getNick() + " " + mess.args[0] + " :No such channel"), serv);
 		return (false);
 	}
-	// user is in it
 	if (!client.isInChannel(mess.args[0]))
 	{
-		client.ConcatenateWBuffer(FormatedMessage("442", ":server", client.getNick() + " " + mess.args[0] + " :You're not on that channel"), serv);
+		client.ConcatenateWBuffer(FormatedMessage("442", ":server",
+			 client.getNick() + " " + mess.args[0] + " :You're not on that channel"), serv);
 		return (false);
 	}
-	// If valid:
-	// broadcast PART to channel
 	Channel &destChan = serv.findChan(mess.args[0], foundChan);
 
 	broadcastMess += ":" + client.getNick() + "!" + client.getUser() + "@host";
@@ -216,7 +214,6 @@ bool	partCmd(Client &client, iRCMessage &mess, Server &serv)
 	broadcastMess += CRLF;
 	client.ConcatenateWBuffer(broadcastMess, serv);
 	serv.broadcast(broadcastMess, client, destChan, serv);
-	// remove user from channel
 	destChan.removeClient(&client);
 	if (destChan.getMembers().size() == 0)
 		serv.deleteChan(mess.args[0]);
